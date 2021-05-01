ca2c236074bef6f04e4d3a0af372f694d356dbfa
natikos <natikos2000@gmail.com>
2021-05-01 23:17:09 +0300

    Add GameOver screen.

--- client/src/pages/GameOverPage/index.tsx
+++ client/src/pages/GameOverPage/index.tsx
@@ -1,9 +1,26 @@
 import './styles.css';
+import Hand from '../../assets/img/hand.svg';
+import GameManager from '../../helpers/GameManager';
+import { formatMoney } from '../../helpers/utils';
+import ControlButton from '../../components/ControlButton';
+import { useGameFlow } from '../../helpers/hooks';
 
 const MAIN_CLASS = 'gameover-page';
 
 const GameOverPage = () => {
-  return <div className={MAIN_CLASS}>GAME OVER</div>;
+  const { newGame } = useGameFlow();
+  return (
+    <div className={MAIN_CLASS}>
+      <Hand className="hand-icon" />
+      <div className={`${MAIN_CLASS}__content`}>
+        <h2 className={`${MAIN_CLASS}__result`}>
+          <span>Total score:</span>
+          {`$${formatMoney(GameManager.currentLevel?.prize ?? 0)} earned`}
+        </h2>
+      </div>
+      <ControlButton text="Try again" onClick={newGame} />
+    </div>
+  );
 };
 
 export default GameOverPage;
